557d6f6066e152e92ccfdb24adb4042a2ed9d2f8
CraigWang1 <55901251+CraigWang1@users.noreply.github.com>
2020-01-05 18:48:33 -0800

    Add files via upload

---
+++ classes.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  5 18:30:56 2020
+
+@author: craig
+
+Config file to set up classes and their values.
+"""
+
+class Class_Value:
+    classes = {"box": 0} #class values
